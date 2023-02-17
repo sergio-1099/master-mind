fb485040ef1debf9f56a9c496375f70f8797fba9
Sergio Guerra <sergioguerra6188@gmail.com>
2023-02-17 13:34:40 -0500

    Add method to get user input for guess
    Method will allow user to input colors into playerGuess array. Chose
    array because order matters.

--- mastermind.rb
+++ mastermind.rb
@@ -5,6 +5,13 @@ class HumanPlayer
         @playerGuess = Array.new(4)
         @playerCode = Array.new(4)
     end
+
+    def inputPlayerGuess
+        @playerGuess.each_index do |index|
+            print "Enter color #{index} guess: "
+            @playerGuess[index] = gets.chomp
+        end
+    end
 end
 
 
